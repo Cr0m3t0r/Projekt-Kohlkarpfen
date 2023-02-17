b1de18b4094154c0d115cf77c51d4520f3e0237c
Cr0m3t0r <92875987+Cr0m3t0r@users.noreply.github.com>
2023-02-17 01:19:33 +0100

    Tags added

--- server/src/tag/tag.controller.ts
+++ server/src/tag/tag.controller.ts
@@ -1,10 +1,35 @@
-import { Controller} from '@nestjs/common';
-import { Tag } from './model/Tag';
+import {Body, Controller, Delete, Get, Post, Put} from '@nestjs/common';
 import {TagService} from './tag.service';
+import {GetTagDto} from "./dto/GetTagDto";
+import {MessageResultDto} from "./dto/MessageResultDto";
 
 
 @Controller('tag')
 export class TagController {
     constructor(private readonly tagService: TagService) {}
+
+    @Get('/list')
+    async getAll(): Promise<GetTagDto>{
+        const result = await this.tagService.getAll()
+        return result
+    }
+
+    @Post('/create')
+    async createTag(@Body() body:{tag:string}):Promise<MessageResultDto>{
+        const result = await this.tagService.create(body.tag)
+        return result
+    }
+
+    @Delete('/delete')
+    async deleteTag(@Body() body:{tag:string}):Promise<MessageResultDto>{
+        const result = await this.tagService.delete(body.tag)
+        return result
+    }
+
+    @Put('/update')
+    async updateTag(@Body() body:{tag:string}):Promise<MessageResultDto>{
+        const result = await this.tagService.update(body.tag)
+        return result
+    }
 
 }
